35a4e466f6574595d226ba4b3f0132c3f02b17df
Talal Ahmad <talaljalil164@gmail.com>
2026-05-05 23:00:05 +0500

    second commit

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 import Router from 'express'
 import {
-    deleteStudent, updateStudent, getStudent, registerStudent, deleteTeacher, updateTeacher, getTeacher, registerTeacher, addLecture, deleteLecture, getLecture, addCourse, deleteCourse, getCourse, loginAdmin, currentAdmin, logoutAdmin, registerAdmin, getAdmin, deleteAdmin, updateAdmin, registerSuperAdmin, addTimetable, deleteTimetable, getTimetable
+    deleteStudent, updateStudent, getStudent, registerStudent, deleteTeacher, updateTeacher, getTeacher, registerTeacher, addLecture, deleteLecture, getLecture, addCourse, deleteCourse, getCourse, loginAdmin, currentAdmin, logoutAdmin, registerAdmin, getAdmin, deleteAdmin, updateAdmin, registerSuperAdmin, addTimetable, deleteTimetable, getTimetable, resetStudentPassword, resetTeacherPassword, resetAdminPassword
 } from '../controllers/admin.controller.js'
 import { verifyAdminToken } from '../middlewares/auth.middleware.js'
 
@@ -10,11 +10,13 @@ router.route('/student/register').post(registerStudent)
 router.route('/students').get(getStudent)
 router.route('/student/:id').delete(deleteStudent)
 router.route('/student/:id').put(updateStudent)
+router.route('/student/reset-password').post(resetStudentPassword)
 
 router.route('/teacher/register').post(registerTeacher)
 router.route('/teachers').get(getTeacher)
 router.route('/teacher/:id').delete(deleteTeacher)
 router.route('/teacher/:id').put(updateTeacher)
+router.route('/teacher/reset-password').post(resetTeacherPassword)
 
 router.route('/lecture/add').post(addLecture)
 router.route('/lecture/:id').delete(deleteLecture)
@@ -35,5 +37,6 @@ router.route('/admin/:id').put(updateAdmin)
 router.route('/login').post(loginAdmin)
 router.route('/logout').post(verifyAdminToken, logoutAdmin)
 router.route('/me').get(verifyAdminToken, currentAdmin)
+router.route('/admin/reset-password').post(resetAdminPassword)
 
 export default router
